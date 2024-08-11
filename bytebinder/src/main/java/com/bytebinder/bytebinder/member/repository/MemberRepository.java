334715204666ff1517561be3388f7d76012b8771
김은비 <eunbi@gim-eunbiui-noteubug.local>
2024-08-11 13:31:14 +0900

    Db연결 변경

--- bytebinder/src/main/java/com/bytebinder/bytebinder/member/repository/MemberRepository.java
+++ bytebinder/src/main/java/com/bytebinder/bytebinder/member/repository/MemberRepository.java
@@ -2,7 +2,11 @@ package com.bytebinder.bytebinder.member.repository;
 
 import com.bytebinder.bytebinder.member.repository.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("select * from dual")
+    public Member findByUsername(String username);
 
 }
